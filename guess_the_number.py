3cbddd71745894a667938698a503bfd4fc7f3036
Igor Brambila <igor_2013@hotmail.com.br>
2024-02-11 11:24:11 -0300

    Guess the Number Game

---
+++ guess_the_number.py
@@ -0,0 +1,22 @@
+import random
+
+total_guesses = 10
+secret_number = random.randint(1, 20)
+
+print('I\'m thinking in a number between 1 and 20 ("Type: use binary search))')
+
+while total_guesses != 0:
+    number_guessed = int(input('Try to guess the number: '))
+    if number_guessed < secret_number:
+        print('Too low')
+    elif number_guessed > secret_number:
+        print('Too high')
+    else:
+        print('I guessed, the number is: ', secret_number)
+        break
+    total_guesses -= 1
+    print('Total guesses left: ', total_guesses)
+if total_guesses == 0:
+    print('Your guesses run out, you lost')
+else:
+    print('Bye bye')
