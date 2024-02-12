2bbcf63879326dfdd1d7c014c0c620a1ec166ade
Igor Brambila <igor_2013@hotmail.com.br>
2024-02-12 11:25:56 -0300

    Improving the code with functions

--- learning_programs/rock_paper_scissors_game.py
+++ learning_programs/rock_paper_scissors_game.py
@@ -1,52 +1,74 @@
-import random, sys
+import random
+import sys
 
-total_wins = 0
-total_loss = 0
-total_ties = 0
-computer_choices = ['r', 'p', 's']
+ROCK = 'rock'
+PAPER = 'paper'
+SCISSORS = 'scissors'
 
-while True:
 
-    computer_chose = random.choice(computer_choices)
-    player_chose = input("Choose: (r)ock, (p)aper, (s)cissors or (q)uit: ").lower()
+def get_computer_choice():
+    return random.choice([ROCK, PAPER, SCISSORS])
 
-    if player_chose == 'r':
-        if computer_chose == 'r':
-            print(f'Computer randomly chose rock, tie')
-            total_ties += 1
-        elif computer_chose == 'p':
-            print(f'Computer randomly chose paper, you lost')
-            total_loss += 1
-        else:
-            print(f'Computer randomly chose scissors, you win')
-            total_wins += 1
 
-    elif player_chose == 'p':
-        if computer_chose == 'r':
-            print(f'Computer randomly chose rock, you win')
-            total_wins += 1
-        elif computer_chose == 'p':
-            print(f'Computer randomly chose paper, tie')
-            total_ties += 1
-        else:
-            print(f'Computer randomly chose scissors, you lost')
-            total_loss += 1
-
-    elif player_chose == 's':
-        if computer_chose == 'r':
-            print(f'Computer randomly chose rock, you lost')
-            total_loss += 1
-        elif computer_chose == 'p':
-            print(f'Computer randomly chose paper, you win')
-            total_wins += 1
-        else:
-            print(f'Computer randomly chose scissors, tie')
-            total_ties += 1
-
-    elif player_chose == 'q':
-        print(f'\nTotal wins: {total_wins}, Total loss: {total_loss}, Total ties: {total_ties}')
-        sys.exit()
+def print_results(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        print(f'You both chose {player_choice}. I\'s a Tie.')
+    elif (player_choice == ROCK and computer_choice == SCISSORS) or \
+         (player_choice == SCISSORS and computer_choice == PAPER) or \
+         (player_choice == PAPER and computer_choice == ROCK):
+        print(f'You choose {player_choice} and the computer choose {computer_choice}. You win!')
+        return 'win'
     else:
-        print('Choose a valid letter: r, p, s or q')
+        print(f'You choose {player_choice} and the computer choose {computer_choice}. You loss!')
+        return 'loss'
 
 
+def transform_player_input(player_choice):
+    choices = {'1': 'rock', '2': 'paper', '3': 'scissors', '4': 'quit'}
+    if player_choice in choices:
+        return choices[player_choice]
+    else:
+        raise ValueError("Invalid input. Please choose a valid number: 1, 2, 3, or 4")
+
+
+def main():
+
+    total_wins = 0
+    total_losses = 0
+    total_ties = 0
+    final_score = 0
+
+    while True:
+        try:
+            player_choice_the_number = input("Choose the number: 1. rock, 2. paper, 3. scissors or 4. quit: ")
+            player_choice = transform_player_input(player_choice_the_number)
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if player_choice not in [ROCK, SCISSORS, PAPER, 'quit']:
+            continue
+
+        if player_choice == 'quit':
+            print(f'\nI\'ve quit the game.\nTotal wins: {total_wins}, Total loss: {total_losses}, Total ties: {total_ties}'
+                  f' = Total Score: {final_score}\n')
+            sys.exit()
+
+        computer_choice = get_computer_choice()
+        results = print_results(player_choice, computer_choice)
+
+        if results == 'win':
+            total_wins += 1
+            final_score += 1
+        elif results == 'loss':
+            total_losses += 1
+            final_score -= 1
+        else:
+            total_ties += 1
+
+        print(f'Total wins: {total_wins}, Total loss: {total_losses}, Total ties: {total_ties}'
+              f' = Total Score: {final_score}\n')
+
+
+if __name__ == '__main__':
+    main()
