55d7fa2f96ad04a552bb6f24db669341eddb9407
Igor Brambila <igor_2013@hotmail.com.br>
2024-02-13 11:00:41 -0300

    adding try/except and 0 to exit

--- learning_programs/collatz_sequence.py
+++ learning_programs/collatz_sequence.py
@@ -1,11 +1,21 @@
+import sys
+
+
 def collatiz(number):
     if number % 2 == 0:
         return number // 2
     if number % 2 == 1:
         return 3 * number + 1
 
-collatiz_number = int(input('Enter a Number: '))
+while True:
+    try:
+        collatiz_number = int(input('Enter a number or 0 to exit: '))
+        if collatiz_number == 0:
+            sys.exit()
+    except ValueError:
+        print('Type a valid number')
 
-while collatiz_number != 1:
-    collatiz_number = collatiz(collatiz_number)
-    print(collatiz_number)
+    while collatiz_number != 1:
+        collatiz_number = collatiz(collatiz_number)
+        print(collatiz_number)
+
